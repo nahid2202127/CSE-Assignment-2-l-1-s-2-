ab78b278ebee892c2468c9da8801ad0747539bf2
nahid2202127 <162349156+nahid2202127@users.noreply.github.com>
2024-03-05 23:59:47 +0600

    Add files via upload

---
+++ 04. Pointeer Data Type.cpp
@@ -0,0 +1,11 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int num = 15;
+   int *p = &num;
+    cout << "Value stored at p: " << *p << endl;
+    cout << "Address stored at p: " << p << endl;
+    cout << "Address stored at p: " << &p << endl;
+}
