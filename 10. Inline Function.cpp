cdbbb9760102c892cdd496191d5105b4e297f3a6
nahid2202127 <162349156+nahid2202127@users.noreply.github.com>
2024-03-06 12:19:31 +0600

    Add files via upload

---
+++ 10. Inline Function.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+inline int area(int l, int w) { return l * w; }
+int main()
+{
+    int length = 10;
+    int wide = 5;
+    cout << "The area is: " << area(length, wide) << endl;
+}
