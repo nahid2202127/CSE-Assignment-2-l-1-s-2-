cad9eb65a469cfeec75a0ded29ef0bfb35286af4
nahid2202127 <162349156+nahid2202127@users.noreply.github.com>
2024-03-13 13:56:03 +0600

    Add files via upload

---
+++ 27.return object.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class sample{
+int a;
+int b;
+public:
+    void setvalue()
+    {
+        a=25;
+        b=40;
+    }
+friend float mean(sample s);
+};
+float mean(sample s)
+{
+    return float (s.a + s.b)/2;
+
+
+}
+int main()
+{
+    sample x;
+    x.setvalue();
+    cout<<"Mean is "<<mean(x)<<endl;
+return 0;}
+
